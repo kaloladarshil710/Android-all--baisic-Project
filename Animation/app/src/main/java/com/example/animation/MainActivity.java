9e45a0bf87bb37d9a41bb1bdf72140f600d27c52
kaloladarshil710 <kaloladarshil710@gmail.com>
2024-07-18 15:38:02 +0530

    1

--- Animation/app/src/main/java/com/example/animation/MainActivity.java
+++ Animation/app/src/main/java/com/example/animation/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.animation;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txtAnim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtAnim=findViewById(R.id.txtAnim);
+
+        Animation move= AnimationUtils.loadAnimation(this,R.anim.move);
 
     }
 }
