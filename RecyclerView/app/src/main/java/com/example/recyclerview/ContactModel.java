0ff72d3f7bdb6b6217ee25ae033087018d9441fe
kaloladarshil710 <kaloladarshil710@gmail.com>
2024-07-19 12:13:02 +0530

    1

---
+++ RecyclerView/app/src/main/java/com/example/recyclerview/ContactModel.java
@@ -0,0 +1,12 @@
+package com.example.recyclerview;
+
+public class ContactModel {
+    int img;
+    String name,number;
+
+    public ContactModel(int img,String name,String number){
+        this.name=name;
+        this.img=img;
+        this.number=number;
+    }
+}
---
+++ RecyclerView/app/src/main/java/com/example/recyclerview/RecyclerContactAdapter.java
@@ -0,0 +1,8 @@
+package com.example.recyclerview;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerContactAdapter extends RecyclerView.Adapter<> {
+
+    public class ViewHolder
+}
