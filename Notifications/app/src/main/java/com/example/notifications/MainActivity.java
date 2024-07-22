a835478daa53e47cf32fa8bd40cd7ca3403d2542
kaloladarshil710 <kaloladarshil710@gmail.com>
2024-07-22 16:00:06 +0530

    completed
    notification is done

--- Notifications/app/src/main/java/com/example/notifications/MainActivity.java
+++ Notifications/app/src/main/java/com/example/notifications/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.notifications;
 
+
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,42 +14,42 @@ import androidx.core.content.res.ResourcesCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private  static final String CHANNAL_ID = "My channal";
-    private  static final int NOTIFICATION_ID = 100;
+    private static final String CHANNAL_ID ="My Channal";
+    private static final int NOTIFICATION_ID =100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         Drawable drawable = ResourcesCompat.getDrawable(getResources(),R.drawable.icon,null);
-        BitmapDrawable bitmapDrawable =(BitmapDrawable) drawable;
 
-        Bitmap largeIcon = bitmapDrawable.getBitmap();
+        BitmapDrawable  bitmapDrawable = (BitmapDrawable) drawable;
+
+        Bitmap  largeIcon = bitmapDrawable.getBitmap();
+
 
         NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
         Notification notification;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                notification = new  Notification.Builder(this)
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            notification = new Notification.Builder(this)
                     .setLargeIcon(largeIcon)
                     .setSmallIcon(R.drawable.icon)
-                    .setContentText("New Text")
-                    .setSubText("New Message from Darshil")
-                    .setChannelId(CHANNAL_ID)
-                    .build();
-                nm.createNotificationChannel(new NotificationChannel(CHANNAL_ID,"New Channal",NotificationManager.IMPORTANCE_HIGH));
+                    .setContentText("N  ew Message")
+                    .setSubText("New Message from RAMAN")
+                    .setChannelId(CHANNAL_ID).build();
+
+            nm.createNotificationChannel(new NotificationChannel(CHANNAL_ID,"522524525252659",NotificationManager.IMPORTANCE_HIGH));
         }else {
-            notification = new  Notification.Builder(this)
+            notification = new Notification.Builder(this)
                     .setLargeIcon(largeIcon)
                     .setSmallIcon(R.drawable.icon)
-                    .setContentText("New Text")
-                    .setSubText("New Message from Darshil")
-                    .build();
-
-            nm.notify(NOTIFICATION_ID,notification);
+                    .setContentText("N  ew Message")
+                    .setSubText("New Message from RAMAN").build();
         }
-
-
+        nm.notify(NOTIFICATION_ID,notification);
     }
+        
 }
