cb712d36350823c7e4d8db427425b0ae950d393a
kaloladarshil710 <kaloladarshil710@gmail.com>
2024-07-22 12:36:15 +0530

    1

--- Notifications/app/src/main/java/com/example/notifications/MainActivity.java
+++ Notifications/app/src/main/java/com/example/notifications/MainActivity.java
@@ -1,24 +1,56 @@
 package com.example.notifications;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.content.res.ResourcesCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private  static final String CHANNAL_ID = "My channal";
+    private  static final int NOTIFICATION_ID = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        Drawable drawable = ResourcesCompat.getDrawable(getResources(),R.drawable.icon,null);
+        BitmapDrawable bitmapDrawable =(BitmapDrawable) drawable;
+
+        Bitmap largeIcon = bitmapDrawable.getBitmap();
+
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification notification;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                notification = new  Notification.Builder(this)
+                    .setLargeIcon(largeIcon)
+                    .setSmallIcon(R.drawable.icon)
+                    .setContentText("New Text")
+                    .setSubText("New Message from Darshil")
+                    .setChannelId(CHANNAL_ID)
+                    .build();
+                nm.createNotificationChannel(new NotificationChannel(CHANNAL_ID,"New Channal",NotificationManager.IMPORTANCE_HIGH));
+        }else {
+            notification = new  Notification.Builder(this)
+                    .setLargeIcon(largeIcon)
+                    .setSmallIcon(R.drawable.icon)
+                    .setContentText("New Text")
+                    .setSubText("New Message from Darshil")
+                    .build();
+
+            nm.notify(NOTIFICATION_ID,notification);
+        }
+
+
     }
 }
