bbbada9a8c2de2356f49fefb947e1aed7c7f446c
kaloladarshil710 <kaloladarshil710@gmail.com>
2024-07-23 14:33:13 +0530

    3

--- TabLayout/app/src/main/java/com/example/tablayout/MainActivity.java
+++ TabLayout/app/src/main/java/com/example/tablayout/MainActivity.java
@@ -2,23 +2,21 @@ package com.example.tablayout;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+TabLayout tab;
+ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        tab = findViewById(R.id.tab);
+        viewPager = findViewById(R.id.viewPager);
+
     }
 }
---
+++ TabLayout/app/src/main/java/com/example/tablayout/ViewPagerMessagerAdepter.java
@@ -0,0 +1,24 @@
+package com.example.tablayout;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class ViewPagerMessagerAdepter extends FragmentPagerAdapter {
+
+    public ViewPagerMessagerAdepter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
